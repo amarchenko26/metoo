491c8125a4e97eac0c2be69e49dc91a37bb020c3
amarchenko26 <31050062+amarchenko26@users.noreply.github.com>
2024-05-28 12:24:17 -0400

    minor update to run run_project

--- run_project.sh
+++ run_project.sh
@@ -2,4 +2,4 @@
 echo “hello world”
 
 # Start every run_project.sh with '#!/bin/bash' or '#!/bin/sh' to tell the system that it is a shell script. Otherwise type which into console. 
-# type chmod +x run_project.sh to change permissions for shell script to be executable by others.
+# After creating shell script, type chmod +x run_project.sh to change permissions for shell script to be executable by others.
