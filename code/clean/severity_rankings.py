40aaa6a0de32f9fc14301c61eb225ef6b69b6991
“jhirsch1” <jacob_hirschhorn@brown.edu>
2024-06-25 10:15:31 -0400

    created severity ranking program

---
+++ code/clean/severity_rankings.py
@@ -0,0 +1,195 @@
+import os
+import getpass
+import io
+import re
+from rapidfuzz import process, fuzz
+from textblob import TextBlob
+
+
+# Get current user ID
+userid = getpass.getuser()
+if userid == "anyamarchenko":
+    root = "/Users/anyamarchenko/CEGA Dropbox/Anya Marchenko/metoo_data"
+elif userid == "maggie":
+    root = "/Users/maggie/Dropbox (Brown)/metoo_data"
+elif userid == "jacobhirschhorn":
+    root = "/Users/jacobhirschhorn/Dropbox (Brown)/metoo_data"
+
+
+
+state = "RI"
+
+# Directory containing the .txt files
+txt_directory = root + "/raw/" + state + "/" + state + "_extracted"
+
+
+def extract_text(input_path : str) -> str:
+    with open(input_path, "r") as text_file:
+        file = text_file.readlines()
+        
+        file = [element for element in file if element != "\n"]
+             
+        for i in range(len(file)):
+            file[i] = file[i].replace('‘', "'")
+            file[i] = file[i].replace('’', "'")
+            file[i] = file[i].replace("£", "E")
+            if file[i][0] == "'":
+                file[i] = file[i].replace("'", '')
+
+        file_str = "\n".join(file)
+
+    file_str = file_str.replace("\n", "")
+
+    return file_str
+
+
+def txt_to_severity_list(text : str) -> tuple[list, int]:
+    # Get the total number of words in the document
+    total_words = text.split()
+
+    # List of relevant words in ascending order of severity
+    word_list = ["Inappropriate behavior", "Lewd comments", "Unwelcome behavior",
+                "Unwanted attention", "Disrespect", "Catcalling", 
+                "Unwanted contact", "Legal action",
+                "Offensiveness", "Objectification", "Intimidation", "Bullying",
+                "Verbal abuse", "Hostile work environment", "Cyber harassment",
+                "Harassment", "Coercion", "Boundary violation", "Stalking",
+                "Exploitation", "Sexual advances", "Discrimination",
+                "Retaliation", "Misconduct", "Sexual misconduct", "Quid pro quo",
+                "Predatory behavior", "Groping", "Non-consensual",
+                "Sexual exploitation", "Threats", "Invasion of privacy", "Trauma",
+                "Power dynamics", "Survivor", "Victimization", "Harasser", 
+                "Human rights", "Workplace harassment", "Sexual harassment", 
+                "Gender-based violence", "Sexual violence", 
+                "Abuse", "Sexual abuse", "Sexual assault", "Assault", "Rape"]
+    
+
+    match_list = []
+    for i in range(len(word_list)):
+        word = word_list[i]
+        word_to_find = word
+
+        # Set threshold - what constitutes a match in the fuzzy match scheme
+        threshold = 75
+
+        # Find out how many words are in the token being checked
+        severity_word_tokens = word.split()
+        severity_word_len = len(severity_word_tokens)
+
+        # Get a list of n-length segments of the text, where n is the length of the word being checked
+        txt_ngrams = generate_ngrams(text, severity_word_len)
+
+        # Search for the word in the document
+        matches = process.extract(word_to_find, txt_ngrams, scorer=fuzz.ratio, limit=None)
+
+        # Filter matches based on the threshold
+        probable_matches = [match for match in matches if match[1] >= threshold]
+
+        # 3-tuple of the word, its ranking, and the amount of that word in the text
+        match_tuple = (word, i+1, len(probable_matches))
+        match_list.append(match_tuple)
+        i += 1
+    
+    return match_list, len(total_words)
+
+
+# Create the n-gram representation of the document
+def generate_ngrams(text, n):
+    words = text.split()
+    ngrams = [' '.join(words[i:i + n]) for i in range(len(words) - n + 1)]
+    return ngrams
+
+# calculate severity score for all txt files
+def get_sev_ranking(match_list) -> tuple[float, float]:
+    severity_ranking = 0
+    total_ranking_words = 0
+    for item in match_list:
+        severity_ranking += item[1]*item[2]
+        total_ranking_words += item[2]
+    
+    return severity_ranking, total_ranking_words
+
+# normalize severity score
+def novel_sev_ranking_list(ranking_list : list, name_list: list):
+    return_list = []
+    max_num = max(ranking_list)
+    # for each case
+    for ind in range(len(ranking_list)):
+        # get the severity ranking for that case
+        num = ranking_list[ind] / max_num
+        # format it into a csv row
+        sev = str(num)
+        return_list.append(sev)
+    return return_list
+
+
+# TextBlob sentiment analysis
+# On a scale of [1, -1] - larger values in either direction are more severe/intense
+# Positive values are positive sentiment, negative values are negative sentiment
+def sentiment_analysis(text : str):
+    # get the text in TextBlob formatting
+    blob = TextBlob(text)
+    sentences = blob.sentences
+    # get the polarity of the text
+    total_polarity = sum(sentence.sentiment.polarity for sentence in sentences)
+    avg_polarity = total_polarity / len(sentences)
+    return avg_polarity
+
+
+
+# output csv
+sev_normalized_list = []
+name_list = []
+sentiment_analysis_list = []
+
+for filename in os.listdir(txt_directory):
+    if filename.endswith(".txt"):
+        txt_path = os.path.join(txt_directory, filename)
+
+        # Get the CSV data from the txt
+        print("Processing", txt_path)
+
+        head, tail = os.path.split(txt_path)
+
+        if not ("RICHR Response to APRA Request" in tail):
+            # get the text of the file
+            text = extract_text(txt_path)
+            # get the severity ranking using novel 
+            sev_list, total_txt_words = txt_to_severity_list(text)
+            sev_number, total_matches = get_sev_ranking(sev_list)
+            # If there is one or more match
+            if total_matches != 0:
+                # calculate the severity two ways
+                # 1. normalized by the number of words in the txt file and by the number of matches found
+                severity_ranking_normalized = (sev_number / total_matches) / total_txt_words
+            # if zero matches, set the severity score to 0
+            else:
+                severity_ranking_normalized = 0
+            name_list.append(tail)
+            sev_normalized_list.append(severity_ranking_normalized)
+
+            # get the severity ranking using sentiment analysis
+            avg_polarity = sentiment_analysis(text)
+            sentiment_analysis_list.append(avg_polarity)
+
+# get the novel severity ranking
+sev_ranking_list = novel_sev_ranking_list(sev_normalized_list, name_list)
+
+
+# combine all parts together
+csv_format_list = ["name,severity_1,severity_2\n"]
+for num in range(len(sev_ranking_list)):
+    row = '"' + name_list[num] + '"' + "," + sev_ranking_list[num] + "," + str(sentiment_analysis_list[num]) + "\n"
+    csv_format_list.append(row)
+
+
+# join into a string
+return_csv = "".join(csv_format_list)
+
+
+output_path = "/Users/jacobhirschhorn/Desktop/RI_sevrankings.csv"
+
+# # Write the CSV to a new file
+with open(output_path, "w") as text_file:
+        text_file.write(return_csv)
+
--- code/clean/txt_to_csv_ri.py
+++ code/clean/txt_to_csv_ri.py
@@ -22,26 +22,43 @@ txt_directory = root + "/raw/" + state + "/" + state + "_extracted"
 
 
 def txt_to_csv(input_path : str):
-    resp_org = ""
-    case_id = ""
-    file_date = ""
-    res_date = ""
-    relief = 0
-    basis = ""
-    jurisdiction = "employment"
+    csv_line = []
 
     with open(input_path, "r") as text_file:
         file = text_file.readlines()
+        # Make txt file more easily parsed
         for i in range(len(file)):
-            if i >= len(file):
+             if i >= len(file):
                 break
-            if file[i][0] == "'":
-                file[i] = file[i][1:]
-            if file[i] == "\n":
+             if file[i] == "\n":
                 file = file[:i] + file[i+1:]
-            
+
+        for i in range(len(file)):
+            file[i] = file[i].replace('‘', "'")
+            file[i] = file[i].replace('’', "'")
+            if file[i][0] == "'":
+                file[i] = file[i].replace("'", '')
+
+
+    # Get the respondent organization/person
+    resp_org = ""
+    resp_trigger = 0
+    for i in range(20):
+        if resp_trigger == 1:
+            if "respondent" in file[i].lower():
+                resp_org += '"'
+                break
+            else:
+                resp_org += file[i][:-1]
+                resp_org += " "
+
+        if "DECISION AND ORDER" in file[i]:
+            resp_trigger = 1
+            resp_org = '"'
+    csv_line.append(resp_org)
 
     # Get case ID
+    case_id = ""
     for line in file:
         # Get the right line
         if line[:4] == "RICH":
@@ -56,18 +73,30 @@ def txt_to_csv(input_path : str):
                     elif re.search(numbers, split_line[i+2][0]):
                         case_id += split_line[i+1]
                         case_id += split_line[i+2]
+                    # Remove any commas
+                    case_id = case_id.replace(',', '')
                     break
-    
-    # Get the respondent organization/person
-    for i in range(20):
-        if "DECISION AND ORDER" in file[i]:
-            resp_org = '"' + file[i+1][:-1] + '"'
+    csv_line.append(case_id)
 
+    # Get the file date
+    file_date = ""
+    for i in range(len(file)):
+        # Find the right line
+        if file[i][:-1] == "INTRODUCTION":
+            split_line = file[i+1].split(",")
+            file_date = '"' + split_line[0].split()[1] + ' ' + split_line[0].split()[2] + ' '
+            file_date += split_line[1] + '"'
+            break
+    csv_line.append(file_date)
+    
     # Get the resolution date
+    res_date = ""
     head, tail = os.path.split(input_path)
     res_date += tail.split()[0]
+    csv_line.append(res_date)
 
     # Get the relief
+    relief = -1
     damages_trigger = 0
     for i in range(len(file)):
         # Find the right line
@@ -81,37 +110,116 @@ def txt_to_csv(input_path : str):
                     if token[0] == "$":
                         no_commas = token.replace(",","")
                         relief += int(float(no_commas[1:]))
+    if relief == -1:
+         csv_line.append("")
+    else:
+        csv_line.append(str(relief + 1))
 
     # Get the basis
+    basis = ""
     disc_trigger = 0
     for i in range(len(file)):
         # Find the right line
-        if "discriminat" in file[i]:
+        if file[i][:-1] == "INTRODUCTION":
             disc_trigger = 1
         if disc_trigger == 1:
-            if "because" in file[i]:
-                basis = '"'
-                basis += file[i]
-                basis += file[i+1]
-                basis += file[i+2]
-                basis += '"'
-                break
-    
-    # Get the file date
-    for i in range(len(file)):
-        # Find the right line
-        if file[i][:-1] == "INTRODUCTION":
-            split_line = file[i+1].split(",")
-            file_date = '"' + split_line[0].split()[1] + ' ' + split_line[0].split()[2] + ' '
-            file_date += split_line[1] + '"'
+            basis_string = file[i][:-1]
+            if "age" in basis_string.lower():
+                basis += "Age"
+            elif "sex" in basis_string.lower():
+                basis += "Sex"
+            elif "disability" in basis_string.lower():
+                basis += "Disability"
+            elif "race" in basis_string.lower():
+                basis += "Race"
+            elif "retaliation" in basis_string.lower():
+                basis += "Retaliation"
+            elif "religion" in basis_string.lower():
+                basis += "Religion"
+            elif "nationality" in basis_string.lower():
+                basis += "Nationality"
+            elif "lgbtq" in basis_string.lower():
+                basis += "LGBTQ"
+        if basis != "":
             break
+        if  file[i][:-1] == "JURISDICTION":
+            basis += "Other"
+            break
+    csv_line.append(basis)
+
+    # Get the jurisdiction
+    jurisdiction = "employment"
+    csv_line.append(jurisdiction)
+
+    # Get the win variable
+    if relief != -1:
+        win = 1
+    else:
+        win = -1
+        win_trigger = 0
+        for i in range(len(file)):
+            if "CONCLUSIONS OF LAW" in file[i]:
+                win_trigger = 1
+            if win_trigger == 1:
+                if "not prove" in file[i]:
+                    win = 0
+                elif "failed to prove" in file[i]:
+                    win = 0
+                elif "prove" in file[i]:
+                    win = 1
+                if "discriminate" in file[i]:
+                    break
+    if win == -1:
+        win = ""
+    
+    csv_line.append(str(win))
+    
+
+    # Get the settle variable
+    settle = 0
+    settle_trigger = 0
+    for i in range(len(file)):
+        if file[i][:-1] == "ORDER":
+            settle_trigger = 1
+        if settle_trigger == 1:
+            if "settle " in file[i]:
+                settle = 1
+    csv_line.append(str(settle))
+
+    # Get the court variable
+    court = 0
+    court_trigger = 0
+    for i in range(len(file)):
+        if file[i][:-1] == "ORDER":
+            court_trigger = 1
+        if court_trigger == 1:
+            if "court" in file[i]:
+                if ("appelate" in file[i]) or "federal" in file[i]:
+                    court = 1
+    csv_line.append(str(court))
+
+
+    # Get the victim_f variable
+    victim_f = -1
+    for i in range(len(file)):
+        if "her " in file[i]:
+            victim_f = 1
+            break
+        if "him " in file[i]:
+            victim_f = 0
+            break
+    if victim_f == -1:
+        victim_f = ""
+    csv_line.append(str(victim_f))
+
 
     # Combine all the information
-    csv_line = tail + ',' + resp_org + ',' + case_id + ',' + file_date + ',' + res_date + ',' + str(relief) + ',' + basis + ',' + jurisdiction + '\n'
+    csv_line = ",".join(csv_line)
+    csv_line += "\n"
     return csv_line
     
 
-csv = "path,resp_org,case_id,file_date,res_date,relief,basis,jurisdiction\n"
+csv = "resp_org,case_id,file_date,res_date,relief,basis,jurisdiction,win,settle,court,victim_f\n"
 # Iterate through each PDF file in the directory
 for filename in os.listdir(txt_directory):
     if filename.endswith(".txt"):
@@ -120,13 +228,14 @@ for filename in os.listdir(txt_directory):
         # Get the CSV data from the txt
         print("Processing", txt_path)
         csv_line = txt_to_csv(txt_path)
-        csv += csv_line
+        head, tail = os.path.split(txt_path)
+        if not ("RICHR Response to APRA Request" in tail):
+            csv += csv_line
 
-# Directory to save the extracted csv file - TODO: Maybe don't need?
+# Flie to save the extracted csv
 output_path = root + "/raw/" + state + "/" + state + "_raw_cases.csv"
 
-
-# Wrtie the CSV to a new file
+# Write the CSV to a new file
 with open(output_path, "w") as text_file:
         text_file.write(csv)
 
